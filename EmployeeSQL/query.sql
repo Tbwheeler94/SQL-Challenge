a742768d8cd5c6082dbb069198d505e6d91aefe2
Thomas Wheeler <tbwheeler94@gmail.com>
2020-04-08 21:19:40 -0700

    Sixth Push

--- EmployeeSQL/query.sql
+++ EmployeeSQL/query.sql
@@ -1,3 +1,4 @@
+--Create Employees Tables
 CREATE TABLE "employees" (
     "emp_no" int,
     "birth_date" date,
@@ -8,6 +9,7 @@ CREATE TABLE "employees" (
     PRIMARY KEY ("emp_no")
 );
 
+--Create Employees Salaries Table
 CREATE TABLE salaries (
 emp_no INT NOT NULL,
 salary INT NOT NULL,
@@ -17,6 +19,7 @@ FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 PRIMARY KEY (emp_no, from_date)
 );
 
+--Create Department Manager Table
 CREATE TABLE dept_mananager (
 dept_no VARCHAR,
 emp_no INT,
@@ -26,12 +29,14 @@ FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
 );
 
+--Create Departments Table
 CREATE TABLE departments (
 dept_no VARCHAR,
 dept_name VARCHAR,
 PRIMARY KEY(dept_no)
 );
 
+--Create Department Employees Table
 CREATE TABLE dept_emp (
 emp_no INT,
 dept_no VARCHAR,
@@ -41,6 +46,7 @@ FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
 );
 
+--Create Titles Table
 CREATE TABLE titles (
 emp_no INT,
 title VARCHAR,
@@ -55,29 +61,35 @@ select * from salaries
 ----List the following details of each employee: employee number, 
 ----last name, first name, gender, and salary.
 
+--Select Employee Number, Last Name, First Name, Gender, Salary
 SELECT emp.emp_no,
 emp.last_name,
 emp.first_name,
 emp.gender,
 sal.Salary
+--Join Employees and Salaries tables on employee number
 FROM employees as emp
 LEFT JOIN salaries as sal
 ON (emp.emp_no = sal.emp_no)
 ORDER BY emp.emp_no;
 
 ----List employees who were hired in 1986
+--Select Employee Number, First Name, Last Name, Hire Date
 SELECT emp_no, first_name, last_name, hire_date
 FROM employees
+--Filter for hire dates during 1986
 WHERE hire_date >= '1986-01-01'
   AND hire_date < '1986-12-31';
   
 --List the manager of each department with the following information: 
 --department number, department name, the manager's employee number, 
 --last name, first name, and start and end employment dates.
+
 SELECT f.dept_no, d.dept_name, 
 f.emp_no, e.last_name, 
 e.first_name, f.from_date, f.to_date
 FROM dept_manager as f
+--Join employee, dept_emp, departments tables
 LEFT JOIN departments as d
 ON (d.dept_no = f.dept_no)
 LEFT JOIN employees as e
@@ -87,6 +99,7 @@ ON (e.emp_no = f.emp_no);
 --information: employee number, last name, first name, and department name.
 SELECT e.emp_no, e.last_name, e.first_name, f.dept_name
 FROM employees as e
+--Join employee, dept_emp, departments tables
 LEFT JOIN dept_emp as d
 ON (d.emp_no = e.emp_no)
 LEFT JOIN departments as f
@@ -96,6 +109,7 @@ ON (f.dept_no = d.dept_no);
 --and last names begin with "B."
 SELECT emp_no, first_name, last_name, hire_date
 FROM employees
+--Filter for people with first name Hercules and last names beginning with B
 WHERE first_name = 'Hercules'
   AND last_name LIKE 'B%';
   
@@ -103,34 +117,48 @@ WHERE first_name = 'Hercules'
 ---including their employee number, last name, 
 ---first name, and department name.
 
+--Select emp_no, last_name, first_name from employee and dept_name from departments
 SELECT e.emp_no, e.last_name, e.first_name, f.dept_name
 FROM employees as e
+--Join employee, dept_emp, departments tables
 LEFT JOIN dept_emp as d
 ON (d.emp_no = e.emp_no)
 LEFT JOIN departments as f
 ON (f.dept_no = d.dept_no)
+--Filter by only Sales department
 WHERE dept_name = 'Sales';
 
 --List all employees in the Sales and Development departments, 
 --including their employee number, last name, first name, and department name.
+
+--Select emp_no from employee and dept_name from departments
 SELECT e.emp_no, e.last_name, e.first_name, f.dept_name
 FROM employees as e
+--Join employee, dept_emp, departments tables
 LEFT JOIN dept_emp as d
 ON (d.emp_no = e.emp_no)
 LEFT JOIN departments as f
 ON (f.dept_no = d.dept_no)
+--Filter for people in Sales & Development departments
 WHERE dept_name = 'Sales'
 OR dept_name = 'Development';
 
 --In descending order, list the frequency count of employee last names, 
 --i.e., how many employees share each last name.
 
+--Select and groupby last name column, run count on each last name
 SELECT last_name, 
 COUNT(*)
 FROM employees
 GROUP BY last_name
 ORDER BY employees.count DESC;
 
+---List employee salary by title (Bonus)
+CREATE VIEW title_salary AS
+SELECT titles.title, salaries.salary
+FROM titles
+LEFT JOIN salaries
+ON (salaries.emp_no = titles.emp_no);
 
 
 
